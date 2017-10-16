1ba7c873c82db52543d9fc092ec9c90d3de69320
Jamie Jin <jamiejin91@gmail.com>
2017-10-16 16:52:32 -0400

    Update xJamieJin_HW8.sql

--- xJamieJin_HW8/xJamieJin_HW8.sql
+++ xJamieJin_HW8/xJamieJin_HW8.sql
@@ -200,9 +200,40 @@ JOIN country
 ON (city.country_id = country.country_id);
 
 # 7h
+SELECT category.name, sum(payment.amount) as 'gross_revenue' 
+FROM category
+JOIN film_category
+ON (category.category_id = film_category.category_id)
+JOIN film
+ON (film_category.film_id = film.film_id)
+JOIN inventory
+ON (film.film_id = inventory.film_id)
+JOIN rental
+ON (inventory.inventory_id = rental.inventory_id)
+JOIN payment
+ON (rental.rental_id = payment.rental_id)
+GROUP BY category.name
+ORDER BY gross_revenue DESC LIMIT 5;
 
 # 8a
+CREATE VIEW top_five_genres AS
+  SELECT category.name, sum(payment.amount) as 'gross_revenue' 
+  FROM category
+  JOIN film_category
+  ON (category.category_id = film_category.category_id)
+  JOIN film
+  ON (film_category.film_id = film.film_id)
+  JOIN inventory
+  ON (film.film_id = inventory.film_id)
+  JOIN rental
+  ON (inventory.inventory_id = rental.inventory_id)
+  JOIN payment
+  ON (rental.rental_id = payment.rental_id)
+  GROUP BY category.name
+  ORDER BY gross_revenue DESC LIMIT 5;
 
 # 8b
+SELECT * FROM top_five_genres;
 
 # 8c
+DROP VIEW top_five_genres;
