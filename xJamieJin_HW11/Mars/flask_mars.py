104dcb3a7e33b0d9507d19d583841f17bca4e5ad
Jamie Jin <jamiejin91@gmail.com>
2017-11-27 22:52:58 -0500

    Add files via upload

--- xJamieJin_HW11/Mars/flask_mars.py
+++ xJamieJin_HW11/Mars/flask_mars.py
@@ -1,6 +1,20 @@
-from flask import Flask, jsonify, render_template, redirect
-
-import scrape_mars
-
+from flask import Flask, render_template, redirect
+from pymongo import MongoClient
+from scrape_mars import scrape
 
 app = Flask(__name__)
+client = MongoClient("mongodb://scrape:mars@ds121696.mlab.com:21696/heroku_661n9r22")
+db = client.heroku_661n9r22
+collection = db.scrape_mars
+
+@app.route("/")
+def home():
+    return render_template("index.html", dict=collection.find_one())
+
+@app.route("/scrape")
+def reload():
+    collection.update({"id": 1}, {"$set": scrape()}, upsert = True)
+    return redirect("http://localhost:5000/", code=302)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- xJamieJin_HW11/Mars/scrape_mars.py
+++ xJamieJin_HW11/Mars/scrape_mars.py
@@ -6,9 +6,8 @@ import time
 
 def scrape():
     
-	executable_path = {'executable_path': 'chromedriver'}
+    executable_path = {'executable_path': 'chromedriver'}
     browser = Browser('chrome', **executable_path, headless=True)
-
 
     # Title & Paragraph Scrape
     url_1 = 'https://mars.nasa.gov/news/'
@@ -16,6 +15,7 @@ def scrape():
     soup_1 = BeautifulSoup(browser.html, "html.parser")
     news_title = soup_1.find('div', class_='content_title').find('a').text
     news_p = soup_1.find('div', class_='article_teaser_body').text
+    print('\ntitle & paragraph complete\n')
 
     # Image Scrape
     url_2 = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
@@ -25,6 +25,7 @@ def scrape():
     browser.click_link_by_partial_text('more info')
     soup_2 = BeautifulSoup(browser.html, "html.parser")
     featured_image_url = "https://www.jpl.nasa.gov{}".format(soup_2.find('figure', class_='lede').a['href'])
+    print('\nimage complete\n')
 
     # Weather Scrape
     url_3 = "https://twitter.com/marswxreport?lang=en"
@@ -35,33 +36,36 @@ def scrape():
         first_word = mars_tweet.text.split(" ")[0]
         if first_word == "Sol":
             mars_weather = mars_tweet.text
+            print('\nweather complete\n')
             break
 
     # Facts B Scrape
-	url_4 = "https://space-facts.com/mars/"
+    url_4 = "https://space-facts.com/mars/"
     browser.visit(url_4)
     soup_4 = BeautifulSoup(browser.html, 'html.parser')
     facts_data = soup_4.find('table', class_="tablepress tablepress-id-mars")
     df_facts = pd.read_html(str(facts_data))
     facts_table = df_facts[0].to_html(index=False, header=None, escape=True).replace("\n", "")
+    print('\nfacts complete\n')
 
     # Hemisphere Images Scrape
     url_5 = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
     browser.visit(url_5)
     soup_5 = BeautifulSoup(browser.html, "html.parser")
-    facts_links = soup_5.find('div', class_="collapsible results").find_all('a', href=True)
+    hemi_links = soup_5.find('div', class_="collapsible results").find_all('a', href=True)
     hemisphere_image_urls = []
-    for facts_link in facts_links:
-        r = requests.get('https://astrogeology.usgs.gov'+facts_link['href'])
-        soup_5_temp = BeautifulSoup(r.content)
-        facts_title = soup_5_temp.find('title').text.split(' Enhanced')[0]
-        facts_img = 'https://astrogeology.usgs.gov' + soup_5_temp.find('img',class_='wide-image').get('src')
-        hemisphere_image_urls.append({'title': facts_title, 'img_url':facts_img})
-
+    for hemi_link in hemi_links:
+        browser.visit("https://astrogeology.usgs.gov" + hemi_link['href'])
+        time.sleep(5)
+        soup_5_temp = BeautifulSoup(browser.html, 'html.parser')
+        hemi_title = soup_5_temp.find('h2', class_="title").text
+        hemi_img = 'https://astrogeology.usgs.gov' + soup_5_temp.find('img',class_='wide-image').get('src')
+        hemisphere_image_urls.append({'title': hemi_title, 'img_url':hemi_img})
+    print('\nhemispheres complete\n')
 
     # Creating and Returning Dictionary for API
     final = {
-        "id": 0,
+        "id": 1,
         "news_title": news_title,
         "news_p": news_p,
         "featured_image": featured_image_url,
